466d6412c5f42596c12ba4cbad06f635bcace34d
Charlotte Chang <yellowspiderlove@gmail.com>
2014-08-05 09:19:50 -0400

    Connect with facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['589073617880756'], ENV['a8f7cb2a1dbdba17858ecb99098c0f03']
+  provider :facebook, '589073617880756', 'eb201681db543f2539b5951defb7c48e'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: redirect('/')
-  match 'signout', to: 'sessions#destroy', as: 'signout'
+  # match 'auth/:provider/callback', to: 'sessions#create'
+  # match 'auth/failure', to: redirect('/')
+  # match 'signout', to: 'sessions#destroy', as: 'signout'
 
   get 'dogs/index'
   # The priority is based upon order of creation: first created -> highest priority.
